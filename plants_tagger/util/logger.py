c9c312d41a1715b245d948ea2fa440781fbb8713
joh <strangenewkid@gmail.com>
2019-08-25 02:21:16 +0200

    logger debug file

--- plants_tagger/util/logger.py
+++ plants_tagger/util/logger.py
@@ -1,5 +1,7 @@
 import logging
 
+from plants_tagger.config_local import LOG_SEVERITY_CONSOLE, LOG_SEVERITY_FILE
+
 
 def configure_root_logger():
     """configure the root logger; each module's default (__name__) logger will inherit these settings"""
@@ -8,14 +10,14 @@ def configure_root_logger():
 
     # create file handler
     file_handler = logging.FileHandler('plants.log')
-    file_handler.setLevel(logging.INFO)
+    file_handler.setLevel(LOG_SEVERITY_FILE)
     format_file = '%(asctime)s - %(threadName)-9s - %(funcName)s - %(name)s - %(levelname)s - %(message)s'
     file_handler.setFormatter(logging.Formatter(format_file))
     logger.addHandler(file_handler)
 
     # create console handler
     stream_handler = logging.StreamHandler()
-    stream_handler.setLevel(logging.DEBUG)
+    stream_handler.setLevel(LOG_SEVERITY_CONSOLE)
     format_stream = '%(name)s - %(levelname)s - %(message)s'
     stream_handler.setFormatter(logging.Formatter(format_stream))
     logger.addHandler(stream_handler)
