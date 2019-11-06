e0c326d243a87db6a777b0ca5a7c59dc9c4db454
joh <strangenewkid@gmail.com>
2019-11-06 20:45:32 +0100

    new trait category 'Taxonomy'

--- plants_tagger/config.py
+++ plants_tagger/config.py
@@ -1,4 +1,4 @@
 size_preview_image = (300, 300)
 size_thumbnail_image = (350, 350)
 filter_hidden = True
-TRAIT_CATEGORIES = ['Plant', 'Leaves', 'Flowers', 'Care', 'Habitat', 'Trivia', 'Others']
+TRAIT_CATEGORIES = ['Plant', 'Leaves', 'Flowers', 'Care', 'Habitat', 'Trivia', 'Taxonomy', 'Others']
