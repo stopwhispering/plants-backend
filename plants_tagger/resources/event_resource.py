8a54c32b0a9c75d589a177172a3594319a1cd50b
joh <strangenewkid@gmail.com>
2019-11-16 21:02:08 +0100

    test

--- plants_tagger/resources/event_resource.py
+++ plants_tagger/resources/event_resource.py
@@ -83,6 +83,7 @@ class EventResource(Resource):
         counts = defaultdict(int)
         new_list = []
         for plant_name, events in plants_events_dict.items():
+            logger.info(f'Updating {len(events)} events for plant {plant_name}')
             plant_obj: Plant = get_sql_session().query(Plant).filter(Plant.plant_name == plant_name).first()
             if not plant_obj:
                 throw_exception(f'Plant not found: {plant_name}')
@@ -91,6 +92,7 @@ class EventResource(Resource):
             event_obj: Event
             for event_obj in plant_obj.events:
                 if event_obj.id not in [e.get('id') for e in events]:
+                    logger.info(f'Deleting event {event_obj.id}')
                     get_sql_session().delete(event_obj)
                     counts['Deleted Events'] += 1
 
