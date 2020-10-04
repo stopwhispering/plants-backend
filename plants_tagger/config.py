d82e39729b7589adc7dbd71855e949e423aa0119
Johannes <Johannes>
2020-10-04 17:44:22 +0200

    update resizing settings

--- plants_tagger/config.py
+++ plants_tagger/config.py
@@ -2,7 +2,7 @@ size_preview_image = (300, 300)
 size_thumbnail_image = (350, 350)
 
 # set resizing_size to none to disable resizing and reencoding quality
-resizing_size = (1920, 1200)
+resizing_size = (3440, 1440)
 quality = 82
 
 filter_hidden = True
