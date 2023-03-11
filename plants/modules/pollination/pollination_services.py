52aad364e548ce86e7097a96d448a6ac3fe33e04
Johannes <stopwhispering314@gmail.com>
2023-03-11 13:11:39 +0100

    debug tz server issues

--- plants/modules/pollination/pollination_services.py
+++ plants/modules/pollination/pollination_services.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import logging
 from datetime import datetime
 from typing import TYPE_CHECKING, Final
 
@@ -53,6 +54,8 @@ LOCATION_TEXTS: Final[dict[str, str]] = {
     "indoor_led": "indoor LED",
     "unknown": "unknown location",
 }
+
+logger = logging.getLogger(__name__)
 
 
 async def _read_pollination_attempts(
@@ -347,6 +350,16 @@ async def update_pollination(
 
     updates = pollination_data.dict(exclude={})
     updates["pollinated_at"] = parse_api_datetime(pollination_data.pollinated_at)
+    logger.warning(updates["pollinated_at"])  # todo remove
+    logger.warning(updates["pollinated_at"].tzinfo)  # todo remove
+    logger.warning(
+        datetime.strptime(  # noqa: PGH004,E501,RUF100,DTZ007
+            pollination_data.pollinated_at,  # noqa: PGH004,E501,RUF100,DTZ007
+            FORMAT_API_YYYY_MM_DD_HH_MM,  # noqa: PGH004,E501,RUF100,DTZ007
+        )
+    )  # noqa: PGH004,E501,RUF100,DTZ007
+    # todo remove
+    logger.warning(pollination_data.pollinated_at)  # todo remove
     updates["label_color"] = label_color
     updates["harvest_date"] = parse_api_date(pollination_data.harvest_date)
     updates["germination_rate"] = germination_rate
